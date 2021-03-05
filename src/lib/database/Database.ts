7b08020f88bb10153b637b5f51eb418ecfb9d838
Lioness100 <jchickmm2@gmail.com>
2021-03-05 16:50:11 -0500

    linting

--- src/lib/database/Database.ts
+++ src/lib/database/Database.ts
@@ -13,7 +13,7 @@ export default class Database {
   }
 
   public async init(): Promise<void> {
-    void mongoose
+    mongoose
       .connect(process.env.MONGO_URI!, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
