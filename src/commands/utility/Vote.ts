b3876d73dec33f5c5c00a77b780b132ab3729f41
Lioness100 <jchickmm2@gmail.com>
2021-03-06 17:34:31 -0500

    Added extreme bot list

--- src/commands/utility/Vote.ts
+++ src/commands/utility/Vote.ts
@@ -21,7 +21,8 @@ export default class VoteCommand extends Command {
         [Discord List](https://discord-list.cf/bots/like/809547125397782528)
         [Paradise Bot List](https://paradisebots.net/bots/809547125397782528/vote)
         [Void Bot List](https://voidbots.net/bot/809547125397782528/vote)
-        [Blist](https://blist.xyz/bot/809547125397782528/vote)`
+        [Blist](https://blist.xyz/bot/809547125397782528/vote)
+        [Extreme Bot List](https://discordextremelist.xyz/en-US/bots/809547125397782528/upvote)`
       )
     );
   }
